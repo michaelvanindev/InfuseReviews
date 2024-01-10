865f7bea1edced0f304c0c6971edd46674450bf4
Michael Vanin <michaelvanin116@gmail.com>
2024-01-10 10:52:34 -0330

    first commit

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,22 @@
+function Card({ children, reverse }) {
+  // Condition class
+  return (
+    <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+  )
+  
+  // Conditional styling
+//   return (
+//     <div className='card' style={{
+//       backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff', color: reverse ? '#fff' : '#000'
+//     }}
+//     >
+//       {children}
+//     </div>
+//   )
+}
+
+Card.defaultProps = {
+  reverse: true,
+}
+
+export default Card
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, repellendus aspernatur obcaecati ipsum provident dolor sunt fuga officia consequuntur unde.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, repellendus aspernatur obcaecati ipsum provident dolor sunt fuga officia consequuntur unde.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, repellendus aspernatur obcaecati ipsum provident dolor sunt fuga officia consequuntur unde.',
+  },
+]
+
+export default FeedbackData
